92f580af593a18d4758aa1662f1c70c3b0facf44
kiranjeet kour <kiranjeetkour144@gmail.com>
2024-01-04 13:31:01 +0530

    modified

--- js/index.js
+++ js/index.js
@@ -11,3 +11,39 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+
+
+$(document).ready(function () {
+    // Function to toggle classes when ComputerHeading is clicked
+    $("#computerHeading").click(function () {
+      $("#computer").toggleClass("paperdiv paperdivBlock");
+    });
+
+    // Repeat the above pattern for other sections if needed
+    // Example for ECHeading
+    $("#ECHeading").click(function () {
+      $("#EC").toggleClass("paperdiv paperdivBlock");
+    });
+ 
+    $("#TexHeading").click(function () {
+        $("#Textile").toggleClass("paperdiv paperdivBlock");
+      });
+
+      $("#MLTHeading").click(function () {
+        $("#MLT").toggleClass("paperdiv paperdivBlock");
+      });
+
+
+      $("#OMHeading").click(function () {
+        $("#OMCA").toggleClass("paperdiv paperdivBlock");
+      });
+
+
+
+    
+    
+
+   
+  });
